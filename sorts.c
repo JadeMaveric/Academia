7260913d943d869581fe275e25df5e4ba729355d
Julius Alphonso <juliusalphonso.09@gmail.com>
2018-07-31 15:51:08 +0530

    Implemented Selection Sort

--- sorts.c
+++ sorts.c
@@ -8,21 +8,21 @@ void bubble_sort(int * list, unsigned int size, char verbose)
     enum bool {false, true} sorted = false;
 
     // Bubble Sort does at most k passes
-    for(k = 0; k < size; k++)
+    for(k=0; k<size; k++)
     {
         // Verbose: Output Array
         if(verbose == 'V')
         {
             int j;
             printf("Pass %d:\n", k+1);
-            for(j = 0; j < size; j++)
+            for(j=0; j<size; j++)
             {
                 printf("%d ", list[j]);
             }
             printf("\n");
         }
 
-        for(i = 1; i < limit; i++)
+        for(i=1; i<limit; i++)
         {
             // Check if adjacent elements are not in the correct order
             if( list[i] < list[i-1])
@@ -31,7 +31,7 @@ void bubble_sort(int * list, unsigned int size, char verbose)
                 if(verbose == 'V')
                 {
                     int j;
-                    for(j = 0; j < i-1; j++)
+                    for(j=0; j<i-1; j++)
                         printf("%d ", list[j]);
                     printf("\033[1;31m%d \033[0m", list[i-1]);
                     printf("\033[1;32m%d \033[0m", list[i]);
@@ -65,4 +65,61 @@ void bubble_sort(int * list, unsigned int size, char verbose)
             sorted = true;
         }
     }
+}
+
+void selection_sort(int * list, unsigned int size, char verbose)
+{
+    int i, j;
+    int large;
+
+    for(i=0; i<size; i++)
+    {
+        // Verbose: Output Array
+        if(verbose == 'V')
+        {
+            int j;
+            printf("Pass %d:\n", i+1);
+            for(j=0; j<size; j++)
+            {
+                printf("%d ", list[j]);
+            }
+            printf("\n");
+        }
+
+        for(j=i+1; j<size; j++)
+        {
+            //Ascending Sort
+            if(list[j] < list[i])
+            {
+                // Verbose: Output modifications
+                if(verbose == 'V')
+                {
+                    int k;
+                    for( k=0; k<size; k++)
+                    {
+                        if( k == i )
+                            printf("\033[1;31m%d \033[0m", list[k]);
+                        else if( k == j )
+                            printf("\033[1;32m%d \033[0m", list[k]);
+                        else
+                            printf("%d ", list[k]);
+                    }
+                    printf("\n");
+
+                }
+
+                list[j]  = list[j] + list[i];
+                list[i]  = list[j] - list[i];
+                list[j]  = list[j] - list[i];
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {3, 7, 8, 1, 9, 10, 2, 6};
+    unsigned int size = sizeof(arr)/sizeof(int);
+
+    selection_sort(arr, size, 'V');
 }
